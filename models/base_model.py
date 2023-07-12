1f0ddc51d6f3fea273f691effdcbcd3e9f8184d6
Tassuo7 <abdelhamid.jack@gmail.com>
2023-07-12 08:56:33 -0700

    add comments

--- models/base_model.py
+++ models/base_model.py
@@ -16,18 +16,22 @@ class BaseModel:
     Return: Nothing
     """
     def __init__(self):
+        """Initialisation"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
+        """print str format"""
         return "[{}] ({}) {}".format(self.__class__.__name__,
                                      self.id, self.__dict__)
 
     def save(self):
+        """updates the attribute updated_at with the current datetime"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """returns a dictionary containing all keys/values"""
         obj_copy = self.__dict__.copy()
         obj_copy["__class__"] = self.__class__.__name__
         obj_copy["created_at"] = self.created_at.isoformat()
