34f596928bf662fe34d4c0650b4d140b1bd90649
salmalah <s.salmalahmidi@gmail.com>
2023-07-12 09:27:44 -0700

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -15,11 +15,18 @@ class BaseModel:
         updated_at: assign with the current datetime when an instance created
     Return: Nothing
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialisation"""
+        date_f = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "updated_at" or key == "created_at":
+                    self.__dict__[key] = datetime.strptime(value, date_f)
+                else:
+                    self.__dict__[key] = value
 
     def __str__(self):
         """print str format"""
