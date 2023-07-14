817c424ed61a187a5346c95bf2cf79f497a88757
Tassuo7 <abdelhamid.jack@gmail.com>
2023-07-14 11:14:53 -0700

    parse and create

--- console.py
+++ console.py
@@ -4,19 +4,21 @@ the entry point of the command interpreter
 """
 import cmd
 import sys
-
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
     """
     use the module cmd to implement command interpreter
     """
     prompt = '(hbnb) '
+    """we need to create known classe"""
+    __classe = {"BaseModel"}
 
-    def do_quit(self, arg):
+    def do_quit(self, line):
         """ quit to exit the program """
         return True
 
-    def do_EOF(self, arg):
+    def do_EOF(self, line):
         """ EOF to exit the program """
         return True
 
@@ -24,6 +26,23 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """ shouldn’t execute anything """
         pass
+
+    def parseline(self, line):
+        """splits the line into arguments"""
+        arguments = cmd.Cmd.parseline(self, line)
+        return arguments
+
+    def do_create(self, line):
+        """Creates a new instance of BaseModel
+        saves it (to the JSON file) and prints the id"""
+        argms = self.parseline(line)
+        if len(argms) == 0:
+            print("** class name missing **")
+        elif argms[0] not in HBNBCommand.__classe:
+            print("** class doesn't exist **")
+        else:
+            print(eval(argms[0])().id)
+            storage.save
 
     if __name__ == '__main__':
         HBNBCommand().cmdloop()
