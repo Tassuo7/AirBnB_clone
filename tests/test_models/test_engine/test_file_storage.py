7bede93a0196a27dd4c23c9b9227143553ce6645
salmalah <s.salmalahmidi@gmail.com>
2023-07-15 10:51:35 -0700

    test

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Defines unittests for class file_storage.py
+"""
+
+
+import unittest
+from models.engine.file_storage import FileStorage
+
+
+class TestFileStorage_instantiation(unittest.TestCase):
