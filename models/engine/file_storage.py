a1bb57097025c5f2bd10f656a873c7da59a33a87
salmalah <s.salmalahmidi@gmail.com>
2023-07-15 13:37:13 -0700

    test

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,11 @@ module to defines class FileStorage
 """
 import json
 from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 from models.base_model import BaseModel
 
 
