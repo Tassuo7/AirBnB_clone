98e6f861e49b1be0f3321975ffb2a38aaff81a18
Tassuo7 <abdelhamid.jack@gmail.com>
2023-07-16 19:02:43 -0700

    try with the new parse

--- console.py
+++ console.py
@@ -52,8 +52,23 @@ class HBNBCommand(cmd.Cmd):
 
     def parseln(self, line):
         """Splits the line into arguments"""
-        argms = line.split()
-        return argms
+        """argms = line.split()"""
+        curly_braces = re.search(r"\{(.*?)\}", arg)
+        brackets = re.search(r"\[(.*?)\]", arg)
+        if curly_braces is None:
+            if brackets is None:
+                return [i.strip(",") for i in split(arg)]
+            else:
+                lexer = split(arg[:brackets.span()[0]])
+                retl = [i.strip(",") for i in lexer]
+                retl.append(brackets.group())
+                return retl
+        else:
+            lexer = split(arg[:curly_braces.span()[0]])
+            retl = [i.strip(",") for i in lexer]
+            retl.append(curly_braces.group())
+            return retl
+        """return argms"""
 
     def do_count(self, class_n):
         """retrieve the number of instances of a class"""
